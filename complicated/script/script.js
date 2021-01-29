9cec4f33edb4e3650959d19d0b3a1c71cc3376c7
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-01-29 17:54:11 +0600

    Правки

--- complicated/script/script.js
+++ complicated/script/script.js
@@ -1,7 +1,30 @@
+// let result = 1;
+// let num = 266219;
+// let str = String(num);
+// let arr = str.split('');
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//   result *= arr[i];
+// }
+// console.log(result);
+
+// result = result**3;
+// console.log(result);
+// result = String(result);
+// console.log(result.slice(0, 2)); 
+
+
+// let num = 12345;
+// let str = String(num);
+// let arr = str.split('');
+
+// console.log(arr);
+
 let result = 1;
 let num = 266219;
 num = String(num);
-num.split('');
+console.log(num);
+console.log(num.split(''));
 for (let i = 0; i < num.length; i++) {
   result *= num[i];
 }
@@ -10,7 +33,4 @@ console.log(result);
 result = result**3;
 console.log(result);
 result = String(result);
-result.split('');
 console.log(result.slice(0, 2)); 
-
-
