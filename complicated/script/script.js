f8512899a366543ab77be6fd28bd76ea3f531e94
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-01-29 22:27:27 +0600

    Fixed

--- complicated/script/script.js
+++ complicated/script/script.js
@@ -1,30 +1,8 @@
-// let result = 1;
-// let num = 266219;
-// let str = String(num);
-// let arr = str.split('');
-// console.log(arr);
-// for (let i = 0; i < arr.length; i++) {
-//   result *= arr[i];
-// }
-// console.log(result);
-
-// result = result**3;
-// console.log(result);
-// result = String(result);
-// console.log(result.slice(0, 2)); 
-
-
-// let num = 12345;
-// let str = String(num);
-// let arr = str.split('');
-
-// console.log(arr);
-
 let result = 1;
 let num = 266219;
 num = String(num);
+num = num.split('');
 console.log(num);
-console.log(num.split(''));
 for (let i = 0; i < num.length; i++) {
   result *= num[i];
 }
@@ -34,3 +12,25 @@ result = result**3;
 console.log(result);
 result = String(result);
 console.log(result.slice(0, 2)); 
+
+
+// // let num = 12345;
+// // let str = String(num);
+// // let arr = str.split('');
+
+// // console.log(arr);
+
+// let result = 1;
+// let num = 266219;
+// num = String(num);
+// console.log(num);
+// console.log(num.split(''));
+// for (let i = 0; i < num.length; i++) {
+//   result *= num[i];
+// }
+// console.log(result);
+
+// result = result**3;
+// console.log(result);
+// result = String(result);
+// console.log(result.slice(0, 2)); 
