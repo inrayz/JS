fe331396817fec6c3563adf2596b867b2d04641d
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-02-05 13:13:51 +0600

    complicated 4

--- lesson/script/script.js
+++ lesson/script/script.js
@@ -1,48 +1,18 @@
-'use strict';
+'use strict'
 
-let isNumber = function (n) {
+const isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+function start (text){
+  text = prompt('Введите строку');
+  if(isNumber(text) || text.trim() === ''){
+    alert('Вы ввели не строку, введите строку');
+    start();
+  }
+  else { 
+    console.log(text.trim().slice(0, 30) + '...');
+  }
+}
+start();
 
-let createGame = function() {
-  let a = Math.floor(Math.random() * 101);
-  console.log(a);
-  
-  let getNumber = function(){
-    let getWish = function(flag){
-      if(flag){
-        getNumber();
-      } else{
-        alert('Спасибо, что поирали');
-      }
-    };
-
-    let wish,
-        userNumber = prompt('Угадайте число от 1 до 100');
-
-    if(userNumber === null){
-      alert('В следующий раз');
-    } 
-    else if(!isNumber(userNumber) || parseFloat(userNumber) > 100){
-      wish = confirm('Введите число от 1 до 100');
-      getWish(wish);
-    }
-    else if (userNumber == a){
-      alert('Ты угадал!!!');
-    }
-    else if (userNumber > a){
-      wish = confirm('Загаданное число меньше. Попробуй еще раз');
-      getWish(wish);
-    }
-    else if (userNumber < a){
-      wish = confirm('Загаданное число больше. Попробуй еще раз');
-      getWish(wish);
-    }
-  };
-  return getNumber;
-};
-
-let game = createGame();
-game();
-console.dir(game);
