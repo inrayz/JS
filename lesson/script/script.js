aa8c0210cfde6366d13d624521bcffe673f7a3f9
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-02-10 19:05:08 +0600

    complicated5

--- lesson/script/script.js
+++ lesson/script/script.js
@@ -1,87 +1,18 @@
-'use strict';
+'use strict'
 
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+let arr = ['32', '27', '755', '404', '67', '78', '277'];
 
-let money;
-
-let start = function() {
-  do{
-    money = prompt('Ваш месячный доход?');
+arr.forEach((item) => {
+  if (item.startsWith('2') || item.startsWith('4')) {
+    console.log(item);
   }
-  while (!isNumber(money));
-  money = +money;
-}; 
-
-start();
-
-let income = 'Фриланс';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let mission = 120000;
-let period = 5;
-
-let showTypeOf = function (data) {
-  console.log(data, typeof(data));
-};
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-let expenses = [];
-
-let temp;
-
-let getExpensesMonth = function () {
-
-  let sum = 0;
-  
-  for (let i = 0; i < 2; i++) {
-    expenses [i] = prompt('Введите обязательную статью расходов?');
-      
-    do{
-      temp = prompt('Во сколько это обойдется?')
-    } while(!isNumber(temp));
-    sum += +temp;
-  }
-  
-  return sum; 
-};
-
-let expensesAmount = getExpensesMonth();
-
-let getAccumulatedMonth = function () {
-  return money - expensesAmount;
-};
-let accumulatedMonth = getAccumulatedMonth();
-
-let budgetDay = Math.floor(accumulatedMonth/30);
-
-let getTargetMonth = function () {
-  return Math.ceil(mission / accumulatedMonth);
-};
+});
 
 
-let getStatusIncome = function () {
-  if (budgetDay >= 1200) {
-    return ('У вас высокий уровень дохода');
-  } else if (600 < budgetDay && budgetDay < 1200) {
-    return ('У вас средний уровень дохода');
-  } else if (0 < budgetDay && budgetDay < 600) {
-    return ('К сожалению у вас уровень дохода ниже среднего');
-  } else if (budgetDay < 0) {
-    return ('Что-то пошло не так');
-  }
-};
-
-console.log('Расходы за месяц:', expensesAmount);
-console.log(addExpenses.toLowerCase().split(','));
-console.log('Бюджет на день:', budgetDay);
-if (getTargetMonth() > 0){
-  console.log('Срок достижения цели в месяцах:', getTargetMonth());
-} else if (getTargetMonth() < 0){
-  console.log('Цель не будет достигнута');
+function isPrime(n) {
+  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
+  return n > 1;
 }
-console.log(getStatusIncome());
+
+const res = [...Array(100)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n');
+console.log(res);
