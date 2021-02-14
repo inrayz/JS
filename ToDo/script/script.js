affb52802e2844b9a791eafc598dd51bbae1d9ae
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-02-14 15:39:41 +0600

    lesson 12

---
+++ ToDo/script/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const todoControl  = document.querySelector('.todo-control'),
+      headerInput = document.querySelector('.header-input'),
+      todoList = document.querySelector('.todo-list'),
+      todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [];  
+
+const render = function(){
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+    todoData.forEach(function(item){
+
+      const li = document.createElement('li');
+      li.classList.add('todo-item');
+
+      li.innerHTML = '<span class="text-todo">' + item.value + '</span>' + '<div class="todo-buttons">' + 
+        '<button class="todo-remove"></button>' + 
+        '<button class="todo-complete"></button>' + 
+      '</div>';
+
+      if(item.completed){
+        todoCompleted.append(li);
+      }else{
+        todoList.append(li);
+      }
+
+      const btnTodoComplete = li.querySelector('.todo-complete');
+
+      btnTodoComplete.addEventListener('click', function(){
+        item.completed = !item.completed;
+        render();
+      });
+
+      const btnTodoRemove = li.querySelector('.todo-remove');
+
+      btnTodoRemove.addEventListener('click', function(){
+        li.remove();
+      });
+      const inputs = document.querySelectorAll('input[type=text]');
+      for (var i = 0;  i < inputs.length; i++) {
+          inputs[i].value = '';
+      }
+    }); 
+};
+
+todoControl.addEventListener('submit', function(event){
+  event.preventDefault();
+  if(headerInput.value === ''){
+    alert('Введите дело!');
+  }else{
+    const newTodo = {
+      value: headerInput.value,
+      completed: false
+    };
+    todoData.push(newTodo); 
+    render();
+  }
+});
+
+render();
+
+localStorage.todoData = JSON.stringify(todoData);
+todoData = localStorage.todoData ? JSON.parse(localStorage.todoData) : [];
