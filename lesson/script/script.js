bb870cc2d8e048c01f4b2500bfdf8f1984a7e20c
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-02-17 15:51:09 +0600

    fix

--- lesson/script/script.js
+++ lesson/script/script.js
@@ -1,75 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+'use strict'; 
+//base
+let DOM = document.querySelector('.DOM');
 
-  'use strict';
 
-  function DomElement(selector, height, width, bg, fontSize) {
+function DomElement (selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
-    this.position = 'absolute';
-    this.positionX = 0;
-    this.positionY = 0;
     this.element = '';
-  }
+}
 
-  DomElement.prototype.create = function () {
+DomElement.prototype.createElement = function () {
+    if (this.selector.startsWith('.')) {
+        this.element = document.createElement('div');
+        this.element.classList.add(`${this.selector.slice(1)}`);
+        this.element.textContent = "Тут должен быть текст";
+        this.element.style.cssText = `height: ${this.height};
+                                    width: ${this.width};
+                                    background: ${this.bg};
+                                    font-size: ${this.fontSize};`;
+                                    
+        document.querySelector('body').append(this.element);
 
-    if (this.selector[0] === '.') {
-      this.element = document.createElement('div');
-      this.element.classList.add(this.selector.slice(1));
+    } else if (this.selector.startsWith('#')) {
+        this.element = document.createElement('p');
+        this.element.setAttribute('id', `${this.selector.slice(1)}`);
+        this.element.textContent = "Тут должен быть текст";
+        this.element.style.cssText = `height: ${this.height};
+                                    width: ${this.width};
+                                    background: ${this.bg};
+                                    font-size: ${this.fontSize};`;
+document.querySelector('body').append(this.element);
     }
+};
 
-    if (this.selector[0] === '#') {
-      this.element = document.createElement('p');
-      this.element.id = this.selector.slice(1);
-    }
-
-    this.element.style.height = this.height;
-    this.element.style.width = this.width;
-    this.element.style.background = this.bg;
-    this.element.style.fontSize = this.fontSize;
-
-    this.element.style.position = this.position;
-    this.element.style.left = this.positionX + 'px';
-    this.element.style.top = this.positionY + 'px';
-
-    this.element.textContent = 'Hello World!';
-    this.element.style.display = 'flex';
-    this.element.style.alignItems = 'center';
-    this.element.style.justifyContent = 'center';
-
-    document.addEventListener('keydown', this.move.bind(this));
-
-    document.querySelector('body').append(this.element);
-
-  };
-
-  DomElement.prototype.reDrow = function () {
-    this.element.remove();
-    this.element.style.left = this.positionX + 'px';
-    this.element.style.top = this.positionY + 'px';
-    document.querySelector('body').append(this.element);
-  };
-
-  DomElement.prototype.move = function (event) {
-    switch (event.key) {
-      case 'ArrowRight':
-        this.positionX += 10;
-        break;
-      case 'ArrowUp':
-        this.positionY -= 10;
-        break;
-      case 'ArrowDown':
-        this.positionY += 10;
-        break;
-      case 'ArrowLeft':
-        this.positionX -= 10;
-        break;
-    }
-    this.reDrow();
-  };
-  let createDiv = new DomElement('.magic', '100px', '100px', 'red', '15px');
-  createDiv.create();
-});
+let domElement = new DomElement('.block', '200px', '300px', 'red', '25px');
+domElement.createElement();
