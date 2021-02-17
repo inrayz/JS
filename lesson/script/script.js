3382764bb7746487b4e993dce0992f7bb179b1de
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-02-17 17:20:03 +0600

    lesson 14-1

--- lesson/script/script.js
+++ lesson/script/script.js
@@ -1,7 +1,6 @@
 'use strict'; 
-//base
-let DOM = document.querySelector('.DOM');
 
+let DOM = document.querySelector('.DOM');
 
 function DomElement (selector, height, width, bg, fontSize) {
     this.selector = selector;
@@ -11,30 +10,26 @@ function DomElement (selector, height, width, bg, fontSize) {
     this.fontSize = fontSize;
     this.element = '';
 }
-
 DomElement.prototype.createElement = function () {
-    if (this.selector.startsWith('.')) {
-        this.element = document.createElement('div');
-        this.element.classList.add(`${this.selector.slice(1)}`);
-        this.element.textContent = "Тут должен быть текст";
-        this.element.style.cssText = `height: ${this.height};
-                                    width: ${this.width};
-                                    background: ${this.bg};
-                                    font-size: ${this.fontSize};`;
-                                    
-        document.querySelector('body').append(this.element);
-
+  if (this.selector.startsWith('.')) {
+    this.element = document.createElement('div');
+    this.element.classList.add(`${this.selector.slice(1)}`);
+    this.element.textContent = "Тут должен быть текст";
+    this.element.style.cssText = `height: ${this.height};
+    width: ${this.width};
+    background: ${this.bg};
+    font-size: ${this.fontSize};`;
+    document.querySelector('body').append(this.element);
     } else if (this.selector.startsWith('#')) {
-        this.element = document.createElement('p');
-        this.element.setAttribute('id', `${this.selector.slice(1)}`);
-        this.element.textContent = "Тут должен быть текст";
-        this.element.style.cssText = `height: ${this.height};
-                                    width: ${this.width};
-                                    background: ${this.bg};
-                                    font-size: ${this.fontSize};`;
-document.querySelector('body').append(this.element);
+    this.element = document.createElement('p');
+    this.element.setAttribute('id', `${this.selector.slice(1)}`);
+    this.element.textContent = "Тут должен быть текст";
+    this.element.style.cssText = `height: ${this.height};
+    width: ${this.width};
+    background: ${this.bg};
+    font-size: ${this.fontSize};`;
+    document.querySelector('body').append(this.element);
     }
 };
-
-let domElement = new DomElement('.block', '200px', '300px', 'red', '25px');
+let domElement = new DomElement(prompt('Введите селектор, который будет начинаться либо с ".", либо с "#"', '.block'), prompt('Введите высоту в пикселях', '100px'), prompt('Введите ширину в пикселях', '100px'), prompt('Введите цвет', 'red'), prompt('Введите размер шрифта в пикселях', '15px'));
 domElement.createElement();
