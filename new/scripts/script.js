f9a817a2be345c75e591694d116f6a9ba0d8b519
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-02-23 16:53:52 +0600

    lesson 18

---
+++ new/scripts/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', function(){
+  'use strict';
+
+
+  //Timer
+  function countTimer(deadline){
+    let timerHours = document.querySelector('#timer-hours'),
+        timerMinutes = document.querySelector('#timer-minutes'),
+        timerSeconds = document.querySelector('#timer-seconds');
+    
+    function getTimeRemaining(){
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+        return{timeRemaining, hours, minutes, seconds};
+    }
+    function pad(n) {
+            if (n < 10){
+                return "0" + n;
+            }
+             return n;
+        }
+    function updateClock(){
+      let timer = getTimeRemaining();
+        timerHours.textContent = pad(timer.hours);
+        timerMinutes.textContent = pad(timer.minutes);
+        timerSeconds.textContent = pad(timer.seconds);
+      if(timer.timeRemaining > 0){
+        setInterval(updateClock, 1000);
+      }else{
+        clearInterval(2);
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+      }
+    }
+    updateClock();
+  }
+  countTimer('24 feb 2021');
+});
